792c25d8a37f56123389241d109822dd68e9ae77
bhanuprakashr51 <bhanuprakashr51>
2024-09-05 16:06:11 +0530

    added print out

---
+++ src/test/java/org/example/GitBasics.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class GitBasics {
+    @Test
+
+    public void print(){
+        System.out.println("Checking basics");
+    }
+}
